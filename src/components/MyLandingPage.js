a5f7836ee300befc594f4d5c5ed2ed1dad549e7a
Olena Vaskevych <olenaj21@gmail.com>
2019-10-21 11:22:48 +0200

    styling changes

--- src/components/MyLandingPage.js
+++ src/components/MyLandingPage.js
@@ -2,31 +2,41 @@ import React, { Component } from "react";
 import logo from "../assets/MYtineraryLogo.png";
 import circle from "../assets/circled-right-2.png";
 import homeicon from "../assets/homeIcon.png";
+import { Link } from "react-router-dom";
 
 class MyLandingPage extends Component {
   state = {};
   render() {
     return (
-      <div className="container">
+      <div className="flexdiv">
         <img alt="company logo" src={logo} className="logo"></img>
-        <p>
-          Find Your perfect trip, designed by insiders who know and love their
-          cities
-        </p>
-        <div className="browsing">
-          <h3>Start Browsing</h3>
-          <img alt="right arrow" src={circle}></img>
-        </div>
-        <div className="login">
-          <p>Want to build your own MYtinerary?</p>
-          <div>
-            <a href="/">Log In</a>
-            <a href="/">Create Account</a>
+
+        <div className="container">
+          <p>
+            Find Your perfect trip, designed by insiders who know and love their
+            cities
+          </p>
+          <div className="browsing">
+            <h3>Start Browsing</h3>
+            <Link to="/CitiesPage">
+              <img alt="right arrow" src={circle}></img>
+            </Link>
           </div>
-          <footer>
-            <img alt="right arrow" src={homeicon}></img>
-          </footer>
+          <div className="login">
+            <h4>Want to build your own MYtinerary?</h4>
+            <div>
+              <Link className="link" to="/LogInPage">
+                Log In
+              </Link>
+              <Link className="link" to="/CreateAccountPage">
+                Create Account
+              </Link>
+            </div>
+          </div>
         </div>
+        <footer className="footer">
+          <img alt="right arrow" src={homeicon}></img>
+        </footer>
       </div>
     );
   }
