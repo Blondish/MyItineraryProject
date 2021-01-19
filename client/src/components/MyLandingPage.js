87ea1fe338edfa1365a1513d5705993e3011018a
Olena Vaskevych <olenaj21@gmail.com>
2021-01-19 19:31:23 +0100

    styled main page

--- client/src/components/MyLandingPage.js
+++ client/src/components/MyLandingPage.js
@@ -1,42 +1,47 @@
 import React, { Component } from "react";
-import logo from "../assets/MYtineraryLogo.png";
-import circle from "../assets/circled-right-2.png";
+import darkBeach from "../assets/DarkBeach.jpg"
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import homeicon from "../assets/homeIcon.png";
 import { Link } from "react-router-dom";
 
 class MyLandingPage extends Component {
   state = {};
-  render() {
-    return (
-      <div className="flexdiv">
-        <img alt="company logo" src={logo} className="logo"></img>
 
+  render() {
+    let backDrop = {
+      backgroundImage: `url(${darkBeach})`, backgroundPosition: "center", backgroundRepeat: "no-repeat",
+      backgroundSize: "cover", height: "100vh"
+    }
+    return (
+      <div className="flexdiv" style={backDrop}>
         <div className="container">
-          <p>
+          <h1>MyTinerary</h1>
+          <h2>
             Find Your perfect trip, designed by insiders who know and love their
             cities
-          </p>
+          </h2>
           <div className="browsing">
             <h3>Start Browsing</h3>
             <Link to="/CitiesPage">
-              <img alt="right arrow" src={circle}></img>
+              <ArrowForwardIosIcon />
             </Link>
           </div>
-          <div className="login">
-            <h4>Want to build your own MYtinerary?</h4>
-            <div>
-              <Link className="link" to="/LogInPage">
-                Log In
+        </div>
+        <div className="login">
+          <h3>Want to build your own MYtinerary?</h3>
+          <div>
+            <Link className="link" to="/LogInPage">
+              Log In
               </Link>
-              <Link className="link" to="/CreateAccountPage">
-                Create Account
+            <Link className="link" to="/CreateAccountPage">
+              Create Account
               </Link>
-            </div>
           </div>
         </div>
-        <footer className="footer">
+
+        {/* <footer className="footer">
           <img alt="right arrow" src={homeicon}></img>
-        </footer>
+        </footer> */}
       </div>
     );
   }
